2adf3d02485e890cdf6c14caa6b0f2f9fd1b6f73
Metin ÖZTAY <metinoztay@gmail.com>
2023-05-03 23:24:23 +0300

    Game UI editted and Cube collecting system added

---
+++ Assets/_Scripts/CubesManager.cs
@@ -0,0 +1,67 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using DG.Tweening;
+
+public class CubesManager : MonoBehaviour
+{
+    private static CubesManager _instance; 
+    [SerializeField] GameUIController gameUIController;
+    [SerializeField] GameObject cubePrefab;
+
+    Queue<GameObject> cubesQueue = new Queue<GameObject>();
+    [SerializeField] Ease easeType;
+    [SerializeField] float duration;
+
+    public static CubesManager Instance 
+    {     
+        get
+        {
+            if (_instance == null)
+            {
+                Debug.LogError("CubesManager Null");
+            }
+            return _instance;
+        }
+    }
+    private void Awake() {
+        _instance = this;
+        PrepareCubes();
+    }
+    private void PrepareCubes(){
+
+        for (int i = 0; i < 50; i++)
+        {
+            GameObject cube;
+            cube = Instantiate(cubePrefab);
+            //* Scale eklenebilir
+            cube.transform.parent = transform;
+            cube.SetActive(false);
+            cubesQueue.Enqueue(cube);
+        }
+    }
+    
+    private void Animate(Vector3 castlePosition){
+        if (cubesQueue.Count>0)
+            {
+                GameObject cube = cubesQueue.Dequeue(); 
+                cube.SetActive(true);
+                cube.transform.position = castlePosition;
+
+                cube.transform.DOMove(transform.position,duration)
+                    .SetEase(easeType)
+                    .OnComplete(()=>{
+                        cube.SetActive(false);
+                        cubesQueue.Enqueue(cube);
+                        gameUIController.cubesCount++;
+                    }
+                );
+            }        
+    }  
+
+    public void AddCube(Vector3 collectedCoinPosition){
+        Animate(collectedCoinPosition);
+    }
+
+}
--- Assets/_Scripts/GameUIController.cs
+++ Assets/_Scripts/GameUIController.cs
@@ -1,21 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using TMPro;
 public class GameUIController : MonoBehaviour
 {
     [SerializeField] Animator cubesHolderAnimator; 
+    [SerializeField] TMP_Text goldsText;
+    [SerializeField] TMP_Text cubesText;
+
+    private int _goldsCount;
+    public int goldsCount
+    {
+        get { return _goldsCount; }
+        set { _goldsCount = value; 
+            goldsText.text = goldsCount.ToString();
+        }
+    }
+    
 
     private int _cubesCount;
     public int cubesCount
     {
         get { return _cubesCount; }
         set { _cubesCount = value; 
+            cubesText.text = cubesCount.ToString();
             cubesHolderAnimator.SetTrigger("Shake");
             }
     }
     
     public void HomeButton(){
-        
+        // leveller arası uı a geçilecek
     }
 }
