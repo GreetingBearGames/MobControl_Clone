e9199d2152b4c11a6ecdcd43eda4d671b9937ac3
Metin ÖZTAY <metinoztay@gmail.com>
2023-05-04 20:35:57 +0300

    Win UI editted & Power UP canvas added without orginal sprites

--- Assets/_Scripts/WinUIController.cs
+++ Assets/_Scripts/WinUIController.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
-
+using UnityEngine.SceneManagement;
 
 public class WinUIController : MonoBehaviour
 {
@@ -36,6 +36,7 @@ public class WinUIController : MonoBehaviour
     }
 
     public void NextButton(){
-        // Leveller arası menüye geçecek
+        int activeLevel = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(activeLevel+1);
     }
 }
